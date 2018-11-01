621a4004fb210a2a58f98e9a958fbfd7f7e64055
alaa <mcse.alaa@gmail.com>
2018-11-01 19:47:54 +0100

    Login and Signup

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ let db; //connecting to heroku or localhost
 if (process.env.DATABASE_URL) {
     db = spicedPg(process.env.DATABASE_URL);
 }else {
-    db = spicedPg("postgres:postgres:postgres@localhost:5432/socialnetwork");
+    db = spicedPg("postgres:postgres:postgres@localhost:5432/todo");
 }
 
 exports.createUser = function(firstName, lastName, email, password) {
